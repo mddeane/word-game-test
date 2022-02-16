dd260973ba0e7b6bc551aa2c0f29e04cf2d99869
Mark Deane <mddeane@yahoo.com>
2022-02-16 13:04:30 -0600

    changes

--- src/com/revature/main/WordGame2.java
+++ src/com/revature/main/WordGame2.java
@@ -1,16 +1,24 @@
 package com.revature.main;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class WordGame2 {
-// change
+
 	private static Scanner sc = new Scanner(System.in); 
 
 	public static String answer = chooseWord();
-
+	public static char[] answerCharArray = answer.toCharArray();
+	
 	public static boolean isCorrectAnswer = false;
 	
 	public static boolean quit = false;
+	
+	public static int rounds = 0; 
+	
+	public static ArrayList<String> guesses = new ArrayList<String>();
+	
+	//public static char[] guessArray = new char[5];
 	
 	public static void main(String[] args) {
 		
@@ -18,17 +26,25 @@ public class WordGame2 {
 		// or if the player quit
 		
 		while (isCorrectAnswer == false && quit == false) {
-			System.out.println("Guess a word.");
+			System.out.println("Guess " + (rounds == 0 ? "a word." : "again. ") + "(Or type \"quit\")");
 			String guess = sc.nextLine();
-			System.out.println("Your guess is " + guess.equals(answer));
 			
 			if (guess.equals(answer)) {
 				quit = true;
+				System.out.println("That's right!");
 				System.out.println("You win!");
 			} else if (guess.equals("quit")) {
 				quit = true;
-				System.out.println("You quit!");
+				System.out.println("You quit after " + rounds + (rounds==1 ? " guess." : " guesses."));			;
+			} else {
+				
+				analyzeGuess(guess);
+				
+				System.out.println("You guessed " + guess);
+				System.out.println("It is not correct.");
+				rounds++;
 			}
+
 		}
 	}
 	
@@ -46,6 +62,20 @@ public class WordGame2 {
 		int randomInt = (int) (Math.random() * words.length);
 		word = words[randomInt];
 		return word;
+	}
+	
+	public static void analyzeGuess(String guess) {
+		char[] guessCharArray = guess.toCharArray();
+		for (char c : guessCharArray) {
+		System.out.println(c);
+		}
+
+		guesses.add(guess);
+		
+		for(String str : guesses) {
+			System.out.println(str);
+		}
+		
 	}
 	
 	
